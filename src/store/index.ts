bb3cb6c029e0c71bf47bfef739f9b136711d47a4
Jared Smith <jared@blacksagesol.com>
2022-06-01 11:52:44 +0300

    Added basic rtk query api for no auth reddit api

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,21 +3,34 @@
  */
 import { configureStore } from '@reduxjs/toolkit'
 
+import { setupListeners } from '@reduxjs/toolkit/query'
+
 import oauthReducer from '@utils/oauth/reducer'
 
-const middleware = []
+import { noauthApi } from '@app/reddit/noauth-api'
 
+import profileReducer from '@app/reddit/profile/reducer'
+
+const middleware: any[] = []
+
+// FIXME: If flipper is not running, the app doesn't start
 if (__DEV__) {
   const createDebugger = require("redux-flipper").default
   middleware.push(createDebugger())
 }
 
 export const store = configureStore({
-  middleware,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(noauthApi.middleware, ...middleware)
+  },
   reducer: {
     oauth: oauthReducer,
+    [noauthApi.reducerPath]: noauthApi.reducer,
+    profile: profileReducer,
   }
 })
+
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch
 
