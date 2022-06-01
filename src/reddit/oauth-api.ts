a8b674ebe4c959cd77cbd384a5e624082d93b1b6
Jessica Smith <5874122+JessicaInTime@users.noreply.github.com>
2022-06-01 11:56:16 +0300

    Moved basic reddit api module to an oauth-api module
    but will look into converting to a rtk query module

--- src/reddit/oauth-api.ts
+++ src/reddit/oauth-api.ts
@@ -2,15 +2,13 @@ import { store } from '@app/store'
 
 /**
  * [redditApi description]
- * @param  {'GET'      |    'POST'}      method [description]
- * @param  {string}        path [description]
- * @return {Promise<void>}      [description]
+ * @param  {'GET' | 'POST'} method [description]
+ * @param  {string}         path   [description]
+ * @return {Promise<void>}         [description]
  */
 function redditApi(method: 'GET' | 'POST', path: string) : Promise<void> {
   const { oauth } = store.getState()
   const { accessToken, tokenType } = oauth
-
-  console.log({oauth})
 
   return fetch(`https://oauth.reddit.com/${path}`, {
     headers: {
