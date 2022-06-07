170d82d7ee38d78cc58781dfc911657d1a8a8bd3
Jared Smith <jared@blacksagesol.com>
2022-06-07 16:59:51 +0300

    Added profile badge/avatar component example, need to figure out what to do with user oauth

---
+++ src/reddit/profile/badge.tsx
@@ -0,0 +1,25 @@
+import { Pressable, StyleSheet, useColorScheme, View } from 'react-native'
+
+const style = StyleSheet.create({
+  container: {
+    // backgroundColor: 'black',  // TODO color theme
+    borderRadius: 100,
+    height: 32,  // TODO spacing (match height in other TopBar items)
+    width: 32,
+  }
+})
+
+export default function ProfileBadge() {
+  const open = () => {
+    console.log('wtf')
+  }
+
+  const isDarkMode = useColorScheme() === 'dark'
+
+  return (
+    <Pressable onPress={open}>
+      <View style={[style.container, {backgroundColor: isDarkMode ? 'white' : 'black'}]}>
+      </View>
+    </Pressable>
+  )
+}
