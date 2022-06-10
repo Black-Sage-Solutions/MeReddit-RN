e1b211f6dd983843879ef322aec3770e6ed08673
Jessica Smith <5874122+JessicaInTime@users.noreply.github.com>
2022-06-10 23:25:06 +0300

    Updated subreddit screen with to show submitted posts for the frontpage and subreddits

--- src/screens/subreddit.tsx
+++ src/screens/subreddit.tsx
@@ -1,26 +1,74 @@
-import { useNavigation } from "@react-navigation/native"
-import { SafeAreaView, StyleSheet, Text, useColorScheme, View } from "react-native"
+/**
+ * 
+ */
 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { useCallback } from 'react'
+
+import {
+    FlatList, StyleSheet,
+    Text, View
+} from 'react-native'
+
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import { RootStackParamList } from '@app/navigation/root'
+
+import { useGetFrontpageQuery, useGetSubredditQuery } from '@app/reddit/noauth-api'
+
+import BaseScreen from '@app/screens/base'
+
+import PostItem from '@components/post-listing'
+
+import NowContext from '@contexts/now'
+
+import EmptyListComponent from '@components/empty-list'
+
+import Loading from '@components/loading'
 
 const style = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'space-between',
-  }
+  list: {
+    marginHorizontal: 8,
+  },
+  separator: {
+    height: 12,
+  },
 })
 
-export default function SubredditScreen() {
-  const isDarkMode = useColorScheme() === 'dark'
+function ItemSeparator() : JSX.Element {
+  return <View style={style.separator} />
+}
 
-  const bgColour = {
-    backgroundColor: isDarkMode ? Colors.black : Colors.white
-  }
+type SubredditScreenProps = NativeStackScreenProps<RootStackParamList, 'Frontpage' | 'Subreddit'>
+
+export default function SubredditScreen({route} : SubredditScreenProps) : JSX.Element {
+  const useSubredditListingQuery = useCallback(() => {
+    if (route.params?.subreddit) {
+      return useGetSubredditQuery(route.params.subreddit)
+    } else {
+      return useGetFrontpageQuery()
+    }
+  }, [route.params?.subreddit])
+
+  const {data, isFetching, isLoading, refetch} = useSubredditListingQuery()
+
+  // Is there actually a performance improvement with providing a now context for PostItem?
+  const nowDate = new Date()
 
   return (
-    <SafeAreaView style={[style.screen, bgColour]}>
-      <Text>Stuff</Text>
-      <Text>Things</Text>
-    </SafeAreaView>
+    <BaseScreen>
+      <NowContext.Provider value={nowDate}>
+        <FlatList
+          ItemSeparatorComponent={() => <ItemSeparator />}
+          ListEmptyComponent={() => (isLoading) ? <Loading /> : <EmptyListComponent />}
+          ListFooterComponent={() => (isLoading) ? null : <Text>TODO pagenate</Text>}
+          data={data?.data?.children || []}
+          keyExtractor={item => item.data.id}
+          onRefresh={refetch}
+          refreshing={isFetching}
+          renderItem={({item}) => <PostItem {...item} />}
+          style={style.list}
+          />
+      </NowContext.Provider>
+    </BaseScreen>
   )
 }
