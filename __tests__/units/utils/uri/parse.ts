08fbc34d78a883cb0c6a4b0d3b1b3d186f18f490
Jared Smith <jared@blacksagesol.com>
2022-06-28 01:48:50 +0300

    Updated utils modules, includes new url output, html unescape, changed util types to interfaces if applied

--- __tests__/units/utils/uri/parse.ts
+++ __tests__/units/utils/uri/parse.ts
@@ -1,51 +1,48 @@
-import { IUri, parse } from '@utils/uri/parse';
+import { parse } from '@utils/uri/parse';
 
-test('parse function for url containing all parts', () => {
-  const test: string = 'mereddit://oauth2redirect/reddit?a=true&b=thing#c=yes&d=no';
-
-  const results: IUri = parse(test);
-
-  expect(results).toStrictEqual({
-    fragment: new Map<string, string | Array<string>>([
-      ['c', 'yes'],
-      ['d', 'no'],
-    ]),
-    host: 'oauth2redirect',
-    path: 'reddit',
-    query: new Map<string, string | Array<string>>([
-      ['a', 'true'],
-      ['b', 'thing'],
-    ]),
-    scheme: 'mereddit',
-  });
-});
-
-test('parse a reddit web address', () => {
-  const test: string = 'https://www.reddit.com/r/linux';
-
-  const results: IUri = parse(test);
-
-  expect(results).toStrictEqual({
-    fragment: new Map(),
-    host: 'www.reddit.com',
-    path: 'r/linux',
-    query: new Map(),
-    scheme: 'https',
-  });
-});
-
-test('multiple same keys in query parameters', () => {
-  const test: string = 'https://www.reddit.com/r/dev?verbose=v&verbose=vv&a=true';
-
-  const results: IUri = parse(test);
-
-  console.log(results);
-
-  expect(results).toStrictEqual({
-    fragment: new Map(),
-    host: 'www.reddit.com',
-    path: 'r/dev',
-    query: new Map(),
-    scheme: 'https',
-  })
-});
+test.each([
+  {
+    test: 'mereddit://oauth2redirect/reddit?a=true&b=thing#c=yes&d=no',
+    expected: {
+      fragment: new Map<string, string>([
+        ['c', 'yes'],
+        ['d', 'no'],
+      ]),
+      host: 'oauth2redirect',
+      path: 'reddit',
+      query: new Map<string, string>([
+        ['a', 'true'],
+        ['b', 'thing'],
+      ]),
+      scheme: 'mereddit',
+    },
+  },
+  {
+    test: 'https://www.reddit.com/r/linux',
+    expected: {
+      fragment: new Map(),
+      host: 'www.reddit.com',
+      path: 'r/linux',
+      query: new Map(),
+      scheme: 'https',
+    },
+  },
+  {
+    test: 'https://www.reddit.com/r/dev?verbose=v&verbose=vv&a=true',
+    expected: {
+      fragment: new Map(),
+      host: 'www.reddit.com',
+      path: 'r/dev',
+      query: new Map<string, string>([
+        ['verbose', 'vv'],
+        ['a', 'true'],
+      ]),
+      scheme: 'https',
+    },
+  },
+])(
+  'Try parsing URI strings to an IUri object',
+  ({test, expected}) => {
+    expect(parse(test)).toStrictEqual(expected);
+  }
+);
--- src/utils/uri/index.d.ts
+++ src/utils/uri/index.d.ts
@@ -2,26 +2,26 @@
  * URI type definitions module.
  */
 
-type FormUrlEncodeObject = {
-  params:     {[key: string]: string},
-  delimiter?: string,
+interface FormUrlEncodeObject {
+  params:     {[key: string]: string}
+  delimiter?: string
 }
 
 interface IUri {
-  fragment: Map<string, string | Array<string>>
+  fragment: Map<string, string[] | string>
   host:     string
   path:     string
-  query:    Map<string, string | Array<string>>
+  query:    Map<string, string[] | string>
   scheme:   string
 }
 
-type UriEncode = {
-  base?:     string,
-  fragment?: Iterable<string, any>,
-  host?:     string,
-  path?:     string,
-  query?:    Iterable<string, any>,
-  scheme?:   string,
+interface IUrl {
+  base?:     string
+  fragment?: Iterable<string, any>
+  host?:     string
+  path?:     string
+  query?:    Iterable<string, any>
+  scheme?:   string
 }
 
-type UriParams = Map<string, string | Array<string>>
+type UriParams = Map<string, string[] | string>
