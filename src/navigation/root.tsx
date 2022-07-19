ab7d786a67b199fa9ab9defe753f25c94a95f81a
Jessica Smith <5874122+JessicaInTime@users.noreply.github.com>
2022-07-20 01:33:44 +0300

    Added tab navigator for the app

--- src/navigation/root.tsx
+++ src/navigation/root.tsx
@@ -1,13 +1,16 @@
-/**
- * 
- */
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer, ParamListBase } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 import CommentsScreen from '@app/screens/comments'
 import SubredditScreen from '@app/screens/subreddit'
 import UserScreen from '@app/screens/user'
+import ProfileScreen from '@app/screens/profile'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { usePalette } from 'ui/palette'
+import SearchComponent from 'components/search'
 
 export interface RootStackParamList extends ParamListBase {
   Comments:  {postId: string, subreddit: string}
@@ -16,20 +19,70 @@ export interface RootStackParamList extends ParamListBase {
   User:      {userName:  string}
 }
 
-const Stack = createNativeStackNavigator<RootStackParamList>()
+const RedditsStack = createNativeStackNavigator<RootStackParamList>()
+
+function RedditsStackScreen() {
+  return (
+    <RedditsStack.Navigator
+      initialRouteName='Frontpage'
+      // TODO figure out what would be best for the header
+      screenOptions={{ headerShown: false }}
+      >
+      <RedditsStack.Screen name='Comments'  component={CommentsScreen} />
+      <RedditsStack.Screen name='Frontpage' component={SubredditScreen} />
+      <RedditsStack.Screen name='Subreddit' component={SubredditScreen} />
+      <RedditsStack.Screen name='User'      component={UserScreen} />
+    </RedditsStack.Navigator>
+  )
+}
+
+interface RootTabParamList extends ParamListBase {
+  Reddits: undefined
+  Profile: undefined
+}
+
+const Tab = createBottomTabNavigator<RootTabParamList>()
 
 export function RootNavigation() : JSX.Element {
+  const palette = usePalette()
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Frontpage'
-        screenOptions={{ headerShown: false }}
+      <Tab.Navigator
+        initialRouteName='Reddits'
+        screenOptions={{
+          headerShown: false,
+          tabBarHideOnKeyboard: true,
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
+          tabBarStyle: {
+            backgroundColor: palette.bgColour,
+          },
+        }}
         >
-        <Stack.Screen name='Comments'  component={CommentsScreen} />
-        <Stack.Screen name='Frontpage' component={SubredditScreen} />
-        <Stack.Screen name='Subreddit' component={SubredditScreen} />
-        <Stack.Screen name='User'      component={UserScreen} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name='Reddits'
+          component={RedditsStackScreen}
+          options={{
+            tabBarIcon: ({color, size}) => <Icon color={color} name='list-alt' size={size} />,
+          }}
+          />
+        <Tab.Screen
+          name='Search'
+          component={SearchComponent}
+          options={{
+            tabBarIcon: ({color, size}) => <Icon color={color} name='search' size={size} />,
+          }}
+          />
+        <Tab.Screen
+          name='Profile'
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({color, size}) => <Icon color={color} name='reddit-alien' size={size} />
+          }}
+          />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
