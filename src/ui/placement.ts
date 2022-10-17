097625ec0438b731064d4ab2347bee4bfd0ec400
Jared Smith <jared@blacksagesol.com>
2022-10-17 16:22:08 +0300

    Added missing explicit typing on placement and typography hook functions

--- src/ui/placement.ts
+++ src/ui/placement.ts
@@ -6,7 +6,7 @@ const defaultConf : Placement = {
   handedness: 'right',
 }
 
-export function useHandedness() {
+export function useHandedness() : Placement {
   // TODO make a user setting
   return defaultConf
 }
